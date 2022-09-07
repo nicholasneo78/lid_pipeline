c83a929b8707886f571b33c0fd62e38b8a8bc3a7
nicholasneo78 <nicholasneo78@gmail.com>
2022-09-07 17:07:06 +0800

    code update

--- tasks/clearml_split_audio_from_manifest.py
+++ tasks/clearml_split_audio_from_manifest.py
@@ -2,6 +2,7 @@ from clearml import Task, Dataset
 
 ### configs for starting clearml ###
 PROJ_NAME = 'LID'
+ITER = 2
 TASK_NAME = 'split_audio_from_manifest'
 DOCKER_IMG = 'nicholasneo78/sb_lid:v0.0.2'
 QUEUE = 'compute'
@@ -9,6 +10,7 @@ QUEUE = 'compute'
 
 ### configs to get the clearml dataset ID ###
 DATASET_ID = 'a8872c8f04444a75b7e1436a72a534e4'
+TRAIN_MANIFEST_ID = 'abeee357e9f14dbbb3729cbfaa032d4c'
 MANIFEST_ID_LIST = ['d54ffb239345410fb7e37e0a43b44ea3', 
                     '898dcc04c9ad476e8071b0d7ffd3b45b']
 
@@ -39,6 +41,10 @@ root_path_list = []
 get_dataset = Dataset.get(dataset_id=DATASET_ID)
 get_dataset_root_path = get_dataset.get_local_copy()
 
+# load the train manifest which includes the english dataset
+get_train_manifest = Dataset.get(dataset_id=TRAIN_MANIFEST_ID)
+get_train_manifest_root_path = get_train_manifest.get_local_copy()
+
 # load the inferred manifest from the previous inference task
 for manifest in MANIFEST_ID_LIST:
     get_manifest = Dataset.get(dataset_id=manifest)
@@ -57,12 +63,23 @@ for idx, root_path in enumerate(root_path_list):
 # create an output folder to save the output as a clearml dataset
 os.mkdir(f'{MANIFEST_ROOT}/')
 
-# call the class to do the shifting of files
+# call the class to do the shifting of files for 'en' class
+shift_en = SplitAudio(manifest_dir=f'{get_train_manifest_root_path}/train_manifest_sb_iteration_{ITER}.json', 
+                      original_root_dir='/lid/datasets/mms/mms_silence_removed', 
+                      replaced_root_dir=f'{get_dataset_root_path}', 
+                      silence_removed_dir=None,
+                      preprocessed_dir=f'{MANIFEST_ROOT}', 
+                      data_is_others=False,
+                      is_clearml=True)
+
+shift_en()
+
+# call the class to do the shifting of files for 'others' class
 for path in full_manifest_path_list:
     shift_others = SplitAudio(manifest_dir=path, 
                               original_root_dir='/lid/datasets/mms/mms_silence_removed', 
                               replaced_root_dir=f'{get_dataset_root_path}', 
-                              silence_removed_dir='mms_silence_removed',
+                              silence_removed_dir=None,
                               preprocessed_dir=f'{MANIFEST_ROOT}', 
                               data_is_others=True,
                               is_clearml=True)
